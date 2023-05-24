a21d7ced0cbe3bec7c37d4e024f7b6e30c646bc2
dsoni868 <dharmik@spiralyze.com>
2023-05-24 11:10:42 +0530

    Demo component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import "./App.css";
-import './index.css'
+import "./index.css";
 import { TrustedSlider } from "./components/TrustedSlider";
 import { Industries } from "./components/Industries";
+import { DemoComponent } from "./components/Demo";
 
 function App() {
-	return (
-		<div style={{ background: 'black', minHeight: '100vh' }}>
-			<TrustedSlider />
-			<hr style={{ background: 'white', margin: '25px 0px 25px 0px' }} />
-			<Industries />
-		</div>
-	);
+  return (
+    <div style={{ background: "black", minHeight: "100vh" }}>
+      <TrustedSlider />
+      <hr style={{ background: "white", margin: "25px 0px 25px 0px" }} />
+
+      <Industries />
+      <hr style={{ background: "white", margin: "25px 0px 25px 0px" }} />
+      <DemoComponent />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Demo.tsx
@@ -0,0 +1,21 @@
+const DemoComponent = () => {
+    return (
+        <div className="demo_section">
+            <div className="demo_section_box">
+                <p className="demo_heading text_white">
+                    Get nTop Demo
+                </p>
+
+                <p className="demo_sub_text text_white_smoke">
+                See how engineers build revolutionary heat exchangers in a fraction of the time.
+                </p>
+
+                <button className="demo_button">
+                    Get a demo
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export { DemoComponent }
